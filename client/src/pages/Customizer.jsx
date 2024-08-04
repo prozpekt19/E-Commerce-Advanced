be01162a94a4c4bd817f329f70e647429cf8a6e4
ATHUL-MURALI <114505380+ATHUL-MURALI@users.noreply.github.com>
2024-08-04 21:56:39 +0530

    side tab slightly created

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { animatePresence, motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import {useSnapshot} from 'valtio';
 
 import config from '../config/config';
@@ -8,11 +8,36 @@ import { download } from '../assets';
 import { downloadCanvasToImage, reader } from '../config/helpers';
 import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
 import { fadeAnimation, slideAnimation } from '../config/motion';
-
+import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
 const Customizer = () => {
+  const snap = useSnapshot(state);
   return (
-    <div>Customizer</div>
+    <div>
+      <AnimatePresence>
+        {!snap.intro && (
+          <>
+            <motion.div
+             key="custom"
+             className='absolute top-0 left-0 z-10'
+             {...slideAnimation('left')} 
+            >
+              <div className='flix item-center min-h-screen'>
+                <div className='editortabs-container tabs'>
+                  {EditorTabs.map((tab) => (
+                    <Tab 
+                      key={tab.name}
+                      tab={tab}
+                      handleClick={() => {}}
+                    />
+                  ))}
+                </div>
+              </div>
+            </motion.div>
+          </>
+        )}
+      </AnimatePresence>
+    </div>
   )
 }
 
